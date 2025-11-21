9ec670f3ba3b15478bb874b637ce0c203cd459fb
Laura02085 <laura.gomez61@est.uexternado.edu.co>
2025-11-21 15:17:47 +0000

    ok

--- .pandas/funciones.py
+++ .pandas/funciones.py
@@ -9,3 +9,23 @@ def promedio(na, nc):
 
     except Exception as e:
         return f"Error al procesar la columna"
+    
+def desviacion(nombre_archivo, nombre_columna):
+    try:
+        if nombre_archivo.endswith(".csv"):
+            df = pd.read_csv(nombre_archivo)
+        elif nombre_archivo.endswith(".xlsx") or nombre_archivo.endswith(".xls"):
+            df = pd.read_excel(nombre_archivo)
+        else:
+            print("Formato de archivo no soportado.")
+            return None
+        
+        if nombre_columna not in df.columns:
+            print("No existe la columna")
+            return None
+        
+        return df[nombre_columna].std()
+
+    except FileNotFoundError:
+        print("No se encontró el archivo.")
+        return None
